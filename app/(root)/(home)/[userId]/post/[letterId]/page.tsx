d172333a78a432fdc8d1c06295b29fdd8bcabb3f
Raymond <akdfhr2@gmail.com>
2023-12-13 02:37:33 +0900

    TODO: 내가받은 편지화면 마크업

--- app/(root)/(home)/[userId]/post/[letterId]/page.tsx
+++ app/(root)/(home)/[userId]/post/[letterId]/page.tsx
@@ -1,10 +1,21 @@
+import Reply from "@/components/reply";
+import { Button } from "@/components/ui/button";
 import { WithParam } from "@/type";
 import React from "react";
 
 const LetterPage = (props: WithParam<"letterId" | "userId">) => {
-  console.log(props.params.letterId);
-  console.log(props.params.userId);
-  return <div>LetterPage</div>;
+  return (
+    <section className="flex flex-col space-y-4 h-full items-center">
+      <h1 className="text-center mt-[10dvh] font-semibold text-2xl mb-4">
+        냥이 1이 보관한
+        <br />
+        첫번 째 편지
+        <br />
+        <Button className="w-full">#1</Button>
+      </h1>
+      <Reply items={["df", "wfwef", "we213123"]} />
+    </section>
+  );
 };
 
 export default LetterPage;
