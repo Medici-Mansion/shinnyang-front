86467352ed7d764f54da347be0f14d0c0d4d3419
ydw1996 <didekdnlt1996@gmail.com>
2023-12-13 21:07:17 +0900

    Revert "stash"
    This reverts commit 605827348523b158ef4578855ca2cf5ab2c63fe7.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,7 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
+        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
       >
         {children}
       </body>
