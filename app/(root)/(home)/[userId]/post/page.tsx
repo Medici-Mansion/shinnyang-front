4404cef08ecff315f51589dd184f7712ecdcda1b
Raymond <akdfhr2@gmail.com>
2023-12-17 01:39:50 +0900

    feat: 우체국 모바일 화면 비율 조정

--- app/(root)/(home)/[userId]/post/page.tsx
+++ app/(root)/(home)/[userId]/post/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const PostPage = () => {
   return (
     <section className="flex flex-col space-y-4 h-full items-center">
-      <h1 className="text-center mt-[10dvh] font-semibold text-2xl mb-4">
+      <h1 className="text-center mt-[7dvh] font-semibold text-2xl mb-4">
         닉네임의
         <br />
         신냥이 우체국
@@ -15,7 +15,7 @@ const PostPage = () => {
           서랍을 누르면 편지를 확인할 수 있어요!
         </sub>
       </h1>
-      <div className="w-full flex flex-col space-y-8 px-4">
+      <div className="w-full flex flex-col space-y-6 px-4">
         <div className="grid grid-cols-3 gap-2 w-full justify-items-center">
           <PostBox text="냥이 1" />
           <PostBox text="냥이 2" />
@@ -30,12 +30,11 @@ const PostPage = () => {
           <PostBox text="#6" />
         </div>
       </div>
-      <div className="w-full grow flex items-end">
-        <div className="relative bg-foreground h-[60%] w-full bottom-0">
-          <div className="relative mx-auto -top-[55%] h-[90%] -z-10 aspect-[31/32]">
-            <Image src="/cat.png" alt="cat" fill />
-          </div>
+      <div className="relative w-full grow flex items-end flex-col">
+        <div className="relative grow mx-auto top-[20%] h-[65%] -z-10 aspect-[31/32]">
+          <Image src="/cat.png" alt="cat" fill />
         </div>
+        <div className="relative bg-foreground h-[60%] w-full bottom-0"></div>
       </div>
       <div className="absolute bottom-10 w-full px-4">
         <Button className="w-full py-6">편지쓰기</Button>
