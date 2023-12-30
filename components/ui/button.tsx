33f7a80b97665649de72aec8c624238ba4243393
Raymond <akdfhr2@gmail.com>
2023-12-30 16:59:12 +0900

    test: 모바일 호버 이펙트 제거

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,11 +9,18 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        primary: "bg-red text-white font-bold hover:bg-primary/90 text-md",
-        secondary: "bg-sub text-red font-bold hover:bg-secondary/80 text-md",
+        // 231230 RAYMOND: 모바일 hover이펙트가 무작위로 활성화 되는 이슈로 인해 호버를 제거한 스타일 적용
+        primary: "bg-red text-white font-bold text-md",
+        secondary: "bg-sub text-red font-bold text-md",
         disable: "bg-gray-300 text-gray-500 font-bold text-md",
-        kakao: "bg-kakao text-black font-bold hover:bg-primary/90 text-md",
-        link: "bg-gray-400 text-black font-bold hover:bg-primary/90",
+        kakao: "bg-kakao text-black font-bold text-md",
+        link: "bg-gray-400 text-black font-bold",
+        // 원본스타일
+        // primary: "bg-red text-white font-bold hover:bg-primary/90 text-md",
+        // secondary: "bg-sub text-red font-bold hover:bg-secondary/80 text-md",
+        // disable: "bg-gray-300 text-gray-500 font-bold text-md",
+        // kakao: "bg-kakao text-black font-bold hover:bg-primary/90 text-md",
+        // link: "bg-gray-400 text-black font-bold hover:bg-primary/90",
       },
       size: {
         default: "w-full h-11 max-h-[54px] rounded-md",
