8478c9b4f99fef1ed70018ec0721e9d6e3c60c5d
Raymond <akdfhr2@gmail.com>
2023-12-31 23:11:25 +0900

    feat: 배너추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { SessionProvider } from "@/components/provider/session-provider";
 import QueryProvider from "@/components/provider/query-provider";
 import localFont from "next/font/local";
 import { userAction } from "@/actions/user-action";
-import Loading from "@/components/loading";
 
 const umu = localFont({
   adjustFontFallback: "Arial",
@@ -108,10 +107,33 @@ export default async function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#111111" />
+        <meta name="msapplication-TileColor" content="#111111" />
+        <meta name="theme-color" content="#111111" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0 , maximum-scale=1.0, user-scalable=no"
         />
+
+        <meta property="og:image" content="/og/banner.png" />
       </head>
       <body
         className={cn("relative font-sans antialiased", pretendard.className)}
