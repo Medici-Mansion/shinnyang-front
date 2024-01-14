749f19bf886274c32b9cf563f97314008223cb40
Raymond <akdfhr2@gmail.com>
2024-01-14 20:14:26 +0900

    feat: 카카오 공유하기 추가

--- env.d.ts
+++ env.d.ts
@@ -6,5 +6,7 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_API_URL: string;
     readonly NEXT_PUBLIC_KAKAO_REDIRECT_URL: string;
     readonly NEXT_PUBLIC_KAKAO_CLIENT_ID: string;
+    readonly NEXT_PUBLIC_KAKAO_SHARE_TEMPLATE_ID: string;
+    readonly NEXT_PUBLIC_KAKAO_JS_KEY: string;
   }
 }
