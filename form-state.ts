3593cab34b2429fcf3ed781c76ebfa7225b3a936
Raymond <akdfhr2@gmail.com>
2024-01-31 23:46:05 +0900

    feat: 편지 공유 프로세스 이슈

--- form-state.ts
+++ form-state.ts
@@ -10,9 +10,15 @@ export const letterFormState = z.object({
     .string()
     .min(1, { message: "내용을 적어주세요!" })
     .max(100, { message: "내용은 100자를 넘을 수 없어요." }),
-  receiverNickname: z.string().min(1, { message: "받는 사람을 입력해주세요!" }),
+  receiverNickname: z
+    .string()
+    .min(1, { message: "받는 사람을 입력해주세요." })
+    .max(14, { message: "받는 사람의 이름이 너무 길어요." }),
   receiverId: z.string().optional(),
-  senderNickname: z.string().min(1, { message: "보내는 사람을 입력해주세요!" }),
+  senderNickname: z
+    .string()
+    .min(1, { message: "보내는 사람을 입력해주세요." })
+    .max(14, { message: "보내는 사람의 이름이 너무 길어요." }),
   letterType: z.enum([LETTER_TYPE.ANSWER, LETTER_TYPE.LETTER]),
   replyMailId: z.string().optional(),
 });
