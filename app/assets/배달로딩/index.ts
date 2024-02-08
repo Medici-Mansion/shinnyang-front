6b1583f8ad694fae51ca10b35463e94e4939a65a
Raymond <akdfhr2@gmail.com>
2024-02-09 02:34:39 +0900

    0.3.6

--- app/assets/배달로딩/index.ts
+++ app/assets/배달로딩/index.ts
@@ -1,9 +1,9 @@
-import 배달1 from "@/app/assets/배달로딩/배달 로딩_1.png";
-import 배달2 from "@/app/assets/배달로딩/배달 로딩_2.png";
-import 배달3 from "@/app/assets/배달로딩/배달 로딩_3.png";
-import 배달4 from "@/app/assets/배달로딩/배달 로딩_4.png";
-import 배달5 from "@/app/assets/배달로딩/배달 로딩_5.png";
-import 배달6 from "@/app/assets/배달로딩/배달 로딩_6.png";
+import 배달1 from "@/app/assets/배달로딩/1.png";
+import 배달2 from "@/app/assets/배달로딩/2.png";
+import 배달3 from "@/app/assets/배달로딩/3.png";
+import 배달4 from "@/app/assets/배달로딩/4.png";
+import 배달5 from "@/app/assets/배달로딩/5.png";
+import 배달6 from "@/app/assets/배달로딩/6.png";
 
 const loading = [배달1, 배달2, 배달3, 배달4, 배달5, 배달6];
 export default loading;
