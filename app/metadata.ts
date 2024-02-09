e8540f9994d035777cc7b7d8e38632afa32334ed
LeeBonHoon <80507816+LeeBonHoon1@users.noreply.github.com>
2024-02-10 00:39:54 +0900

    fix: 링크공유 이미지 수정

--- app/metadata.ts
+++ app/metadata.ts
@@ -25,15 +25,14 @@ export const defaultMetadata: Metadata = {
   openGraph: {
     type: "website",
     countryName: "korea",
-    description:
-      "귀여운 고양이 '우무', '체즈', '구키'를 통해 새해 편지를 공유하고 활기찬 한해를 시작해 보세요!",
+    description: "설날 편지 대신 주러간다냥",
     emails: "medici-ideas@gmail.com",
-    title: "새해를 알리는 2024 새해복설냥",
+    title: "새해복설냥",
     url: "https://catsnewyear.site",
     locale: "ko_KR",
     siteName: "새해복설냥",
     images: [
-      "https://res.cloudinary.com/dzfrlb2nb/image/upload/v1707479297/o2ghfvd9vqprddzbpqmg.png",
+      "https://res.cloudinary.com/dzfrlb2nb/image/upload/v1707493136/fityfqwfp5rux3nrbnwx.png",
       "https://res.cloudinary.com/dzfrlb2nb/image/upload/v1707477052/uvt55nfusidzaf3c8gm7.png",
     ],
   },
